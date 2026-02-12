1763d4b9d2240f34f10ae1325fb8c203492c42b7
antnpwr <nopwaree@gmail.com>
2026-02-12 11:33:48 +0700

    fix code to make the test pass

--- index.js
+++ index.js
@@ -83,7 +83,10 @@ app.put('/api/todos/:id', (req, res) => {
     return res.status(404).json({ error: 'Todo not found' });
   }
   
-  todos[todoIndex].completed = true;
+  todos[todoIndex].completed = !todos[todoIndex].completed;
+
+  writeTodos(todos);
+  res.status(200).json(todos[todoIndex]);
 });
 
 // Delete a todo
